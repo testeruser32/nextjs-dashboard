a6cbf32292acb343abf81d70ee33576c78b9e688
testeruser32 <lgnnnkxp@gmail.com>
2025-04-12 14:24:39 -0300

    Minha primeira mensagem de commit

---
+++ app/dashboard/oneTimeTwin/page.tsx
@@ -0,0 +1,63 @@
+export default function Page() {
+  return( 
+    <div>
+      <ul>
+        <li className="p-10"><strong>1 (Segunda)</strong>
+          <ul>
+            <li>1-Hamstring - 30 seg</li>
+            <li>2-Sumô - 5x15</li>
+            <li>3-Plank - 3-4x 30 seg (resfriado)</li>
+            <li>4-Flexão com joelho - 1x 15 à 20</li>           
+            <li><em>Opcional:(ou seja nao)</em>
+              <ul>
+                <li>5-Pendurar - 3-4x 20-30 seg (2 mãos apenas, sem forçar e focar costas)</li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+
+        <li className="p-10"><strong>2 (Quarta)</strong>
+          <ul>
+            <li>1-Hamstring - 30 seg</li>
+            <li>2-Sumô - 5x15</li>
+            <li>3-Twin stiff - 3-4x12</li>
+            <li>4-Calf Twin - 3-4x 40-60</li>
+            <li>5-Voador dorsal - 2 à 5x 5 à 10</li>
+            <li>6-Pendurar - 3-4x 20-30 seg (2 mãos apenas, sem forçar e focar costas)</li>
+            <li><em>Opcional:(ou seja nao)</em>
+              <ul>
+                <li>7-Plank - 3-4x 30 seg (resfriado)</li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+
+        <li className="p-10"><strong>3 (Sábado)</strong>
+          <ul>
+            <li>1-Hamstring - 30 seg</li>
+            <li>2-Sumô - 5x15</li>
+            <li>3-Twin Bíceps - 3-4x12 (4 - 6 - 8 kg)</li>
+            <li>4-Elevação lateral - 3-4x 8 à 12 (4 - 6 - 8 kg)</li>
+            <li>5-Voador dorsal - 2 à 5x 5 à 10</li>
+            <li>6-Pendurar - 3-4x 20-30 seg (2 mãos apenas, sem forçar e focar costas)</li>
+            <li>7-Liberação miofascial (panturrilha, posterior de coxa, glúteos e lombar)</li>
+          </ul>
+        </li>
+
+        <li className="p-10"><strong>4 (Domingo)</strong>
+          <ul>
+            <li>1-Hamstring - 30 seg</li>
+            <li>2-Sumô - 5x15</li>
+            <li><em>Opcional:(ou seja nao)</em>
+              <ul>
+                <li>3-Pendurar - 3-4x 20-30 seg (2 mãos apenas, sem forçar e focar costas)</li>
+                <li>4-Plank - 3-4x 30 seg (resfriado)</li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
